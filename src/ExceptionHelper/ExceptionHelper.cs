1c5b24f30fae6d2662da3ec2dec97bfd818ead5b
René Emmanuel Zamorano Flores <final20@gmail.com>
2020-06-01 10:53:01 -0500

    Added basic functionalitiy

---
+++ src/ExceptionHelper/ExceptionHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHelper
+{
+    public class ExceptionHelper
+    {
+        private readonly Dictionary<Type, Action<Exception>> _behaviors = new Dictionary<Type, Action<Exception>>();
+
+        public T Try<T>(Func<T> function)
+        {
+            try
+            {
+                return function.Invoke();
+            }
+            catch ( Exception e )
+            {
+                HandleException(e);
+            }
+
+            return default;
+        }
+
+        public void Try(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch ( Exception e )
+            {
+                HandleException(e);
+            }
+        }
+
+        private void HandleException(Exception e)
+        {
+            if (_behaviors.ContainsKey(e.GetType()))
+                _behaviors[e.GetType()].Invoke(e);
+            else 
+                OnException(e);
+
+        }
+
+        protected virtual void OnException(Exception e)
+        {
+            throw e;
+        }
+
+        public void AddBehaviorFor(Type type, Action<Exception> handler)
+        {
+            try
+            {
+                _behaviors.Add(type, handler);
+            }
+            catch (ArgumentException e)
+            {
+                throw new InvalidOperationException($"A behavior for {type.Name} exception has been already defined.");
+            }
+        }
+
+        public void AddBehaviorFor<T>(Action<Exception> behavior)
+        {
+            AddBehaviorFor(typeof(T), behavior);
+        }
+    }
+}
---
+++ src/NUnit.Tests/ExceptionHelperTests.cs
@@ -0,0 +1,162 @@
+﻿using NUnit.Framework;
+using ExceptionHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHelper.Tests
+{
+    [TestFixture()]
+    public class ExceptionHelperTests
+    {
+        /// <summary>
+        /// Given: code is called using try
+        /// When: code is provided
+        /// Then: should execute the providen code
+        /// </summary>
+        [Test()]
+        public void Try_ShouldExecuteGivenCode()
+        {
+            // arrange
+            var helper = new ExceptionHelper();
+            var current = false;
+            var expected = true;
+
+            // act
+            helper.Try(() => current = true);
+
+            // assert
+            Assert.That(current, Is.EqualTo(expected));
+        }
+
+        /// <summary>
+        /// Given: a catch behavior is defined for an exception
+        /// When: executed code throws an exception
+        /// Then: should execute providen code for exception
+        /// </summary>
+        [Test()]
+        public void Try_ShouldExecuteExceptionCode()
+        {
+            // arrange
+            var helper = new ExceptionHelper();
+            var current = false;
+            var expected = true;
+
+            helper.AddBehaviorFor(typeof(Exception), (e) => current = true);
+
+            // act
+            helper.Try(() => throw new Exception());
+
+            // assert
+            Assert.That(current, Is.EqualTo(expected));
+        }
+
+        /// <summary>
+        /// Given: a catch behavior is defined for an exception
+        /// When: executed code throws an exception
+        /// Then: pass the original exception to callback code
+        /// </summary>
+        [Test()]
+        public void Try_ShouldPassOriginalExceptionParameter()
+        {
+            // arrange
+            var helper = new ExceptionHelper();
+            Exception current = null;
+            var expected = new Exception("Test exception");
+
+            helper.AddBehaviorFor(typeof(Exception), (e) => current = e);
+
+            // act
+            helper.Try(() => throw expected);
+
+            // assert
+            Assert.That(current, Is.EqualTo(expected));
+        }
+
+        /// <summary>
+        /// Given: a catch behavior is defined for an exception
+        /// When: executed code throws a not defined exception
+        /// Then: rethrow the not defined exception
+        /// </summary>
+        [Test()]
+        public void Try_ShouldRethrowANotDefinedException()
+        {
+            // arrange
+            var helper = new ExceptionHelper();
+            var expected = new ArgumentException("Test exception");
+
+
+            // act
+
+
+            // assert
+            Assert.Throws<ArgumentException>(() => helper.Try(() => throw expected));
+        }
+
+        /// <summary>
+        /// Given: a catch behavior is defined for an exception
+        /// When: user try to add same behavior twice
+        /// Then: throws InvalidOperationException 
+        /// </summary>
+        [Test()]
+        public void Try_ShouldThrowInvalidOperationExceptionWhenTryToAddRepeatedBehavior()
+        {
+            // arrange
+            var helper = new ExceptionHelper();
+            Exception current = null;
+            var expected = new Exception("Test exception");
+
+            helper.AddBehaviorFor(typeof(Exception), (e) => current = e);
+
+
+            // act
+            Assert.Throws<InvalidOperationException>(() =>
+                helper.AddBehaviorFor(typeof(Exception), (e) => current = e));
+        }
+
+        /// <summary>
+        /// Given: a catch behavior is defined for an exception with the Generic Method
+        /// When: executed code throws an exception
+        /// Then: should execute the code for exception
+        /// </summary>
+        [Test()]
+        public void Try_AddBehaviorWithGenericShouldExecuteExceptionCode()
+        {
+            // arrange
+            var helper = new ExceptionHelper();
+            var current = false;
+            var expected = true;
+
+            helper.AddBehaviorFor<Exception>((e) => current = true);
+
+            // act
+            helper.Try(() => throw new Exception());
+
+            // assert
+            Assert.That(current, Is.EqualTo(expected));
+        }
+
+        /// <summary>
+        /// Given: code is called using try
+        /// When: code that returns a value is provided
+        /// Then: should execute the providen code and return the valur
+        /// </summary>
+        [Test()]
+        public void Try_ShouldExecuteGivenCodeAndReturnResult()
+        {
+            // arrange
+            var helper = new ExceptionHelper();
+            var current = false;
+            var expected = true;
+
+            // act
+            current = helper.Try(() => true);
+
+            // assert
+            Assert.That(current, Is.EqualTo(expected));
+        }
+    }
+}
